1d87490f20161fcdc63a05125a47e86e4916efca
itsadmin <itsadmin@192.168.0.138>
2017-11-23 12:47:38 +0100

    OMO

--- Modulo3-01/src/FedericoBenassi.java
+++ Modulo3-01/src/FedericoBenassi.java
@@ -1,7 +1,7 @@
 
 public class FedericoBenassi {
 		public static void main(String [] args) {
-			System.out.println("Aloha sono Federico Benassi");
+			System.out.println("Aloha sono gay");
 		}
 
 }
