2cf10eeddee89a4c80ef5c6f82d1ab87c341628c
Meringone <f.benassi@itsrizzoli.it>
2018-01-16 11:42:56 +0100

    Assenza cambiato in presenza

--- Modulo3-01/src/FedericoBenassi.java
+++ Modulo3-01/src/FedericoBenassi.java
@@ -1,7 +1,13 @@
-
+import java.util.*;
 public class FedericoBenassi {
 		public static void main(String [] args) {
 			System.out.println("Aloha sono Federico Benassi");
+			String x;
+			do  {
+				Scanner sc = new Scanner(System.in);
+				x = sc.nextLine();
+				System.out.println(x);
+			} while (x.equals("ciao"));
 		}
 
 }
