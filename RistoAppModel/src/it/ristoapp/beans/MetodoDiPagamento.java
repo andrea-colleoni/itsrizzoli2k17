b429b160c3256823673119d6e99d341a957df58a
Andrea Colleoni <andrea@colleoni.info>
2018-01-17 12:21:51 +0100

    esempio enum

---
+++ RistoAppModel/src/it/ristoapp/beans/MetodoDiPagamento.java
@@ -0,0 +1,8 @@
+package it.ristoapp.beans;
+
+public enum MetodoDiPagamento {
+	PAGA_SUL_POSTO,
+	PAYPAL,
+	CARTA_DI_CREDITO,
+	POSTEPAY
+}
---
+++ RistoAppModel/src/it/ristoapp/beans/Prenotazione.java
@@ -0,0 +1,11 @@
+package it.ristoapp.beans;
+
+import java.util.Date;
+
+public class Prenotazione {
+	
+	private Date data;
+	
+	private MetodoDiPagamento metodoPagamento;
+
+}
